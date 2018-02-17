3e80f7db8ecad5f69055a617337786982a788302
Bamamanjam Development <bamamanjam.development@zemljak.si>
2018-02-17 12:41:56 +0100

    Circular buffer and recive and send processes added
    CircularBuffer library added.
    Recive and Send processes written and added to the main loop. Not tested yet.

--- Arduino2PiSer.ino
+++ Arduino2PiSer.ino
@@ -10,13 +10,43 @@
 *   
 */
 
+#include <CircularBuffer.h>
 
-void setup() 
+// Buffers
+CircularBuffer<byte,64> SERRxBuffer; 
+CircularBuffer<byte,64> SERTxBuffer;
+
+void fnSERRecive()
+{
+  while (Serial.available() > 0)
+  {
+    SERRxBuffer.push(Serial.read());
+  }
+}
+
+void fnSERSend()
+{
+  while (!SERTxBuffer.isEmpty())
+  {
+    Serial.write(SERTxBuffer.shift());
+  }
+}
+
+void fnProcessData()
 {
   
 }
 
+void setup() 
+{
+  // start serial port
+  Serial.begin(9600);  
+}
+
 void loop() 
 {
-
+  // main loop
+  fnSERRecive();
+  fnProcessData();
+  fnSERSend();
 }
