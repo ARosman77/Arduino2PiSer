2425470ffe4f57d59e466393bb75fb01918bb15e
Bamamanjam Development <bamamanjam.development@zemljak.si>
2018-02-17 12:01:21 +0100

    Initial commit

---
+++ Arduino2PiSer.ino
@@ -0,0 +1,22 @@
+/*
+*   Arduino2PiSer
+*
+*   Serial protocol for communication between OrangePi PC and Arduino. 
+*   Used for simple sensor data requests.
+*   
+*   Test version, to be upgraded to library once done and tested.
+*   
+*   17.2.2018 / inital version 0.1.
+*   
+*/
+
+
+void setup() 
+{
+  
+}
+
+void loop() 
+{
+
+}
