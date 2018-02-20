e22a5b7bd2b685be6a457aba917fc5594376d209
Ales@Zver2017 <ales.rosman@gmail.com>
2018-02-20 22:14:32 +0100

    Protocol parsing rewritten
    Major changes to the protocol.

--- Arduino2PiSer.ino
+++ Arduino2PiSer.ino
@@ -8,7 +8,36 @@
 *   
 *   17.2.2018 / inital version 0.1.
 *   
+*   Protocol idea:
+*   
+*     <[LEN]/[ID*];[CMD]:[PARAM1] [PARAM2] ... [PARAMn]=[CHKSUM*]>
+*               * optional ID                                  * optional checksum (sum%256)
+*     Each of the data is limited to max 16 chars!
+*   
 */
+
+// #define _DEBUG_INIT
+#define _DEBUG_
+
+// allowed ASCII for communication
+const char gcAllowedStart = 32;
+const char gcAllowedEnd = 122;
+
+// start/stop chars
+const char gcStartChar = 60;         // <
+const char gcPreIDChar = 47;         // /
+const char gcPreCMDChar = 59;        // ;
+const char gcPreParamChar = 58;      // :
+const char gcParamSeparator = 32;    // SPACE
+const char gcPreChkSumChar = 61;     // =
+const char gcStopChar = 62;          // >
+
+const byte gcMaxDataPartLen = 16;
+
+// debug LED pins
+const byte pinLED_R = 3;
+const byte pinLED_G = 5;
+const byte pinLED_B = 6;
 
 #include <CircularBuffer.h>
 
@@ -19,7 +48,7 @@ CircularBuffer<byte,64> gSERTxBuffer;
 // State machine
 enum enProcessStates
 {
-  STATE_CLEAN_UP,         // before initial state message clean-up is needed
+  STATE_CLEAN_UP,         // before initial state message clean-up is needed ???
   STATE_IDLE,             // inital state, waiting for packet to start
   STATE_CMD_LEN,          // start-of-packet received, waiting for packet length
   STATE_CMD_ID,           // packet length recived, waiting for ID [optional?]
@@ -32,11 +61,14 @@ enum enProcessStates
   STATE_CMD_ACK,          // message processing finished, answer prepared to be sent
   STATE_CMD_NAK,          // message processing finished, error prepared to be sent
 };
-enum enProcessStates gProcessState = STATE_IDLE;
+enum enProcessStates gProcessState = STATE_CLEAN_UP;
 byte gMsgCheckSum = 0;
 byte gMsgLength = 0;
 byte gMsgCmd = 0;
-CircularBuffer<char,64> gMsgParams;
+CircularBuffer<int,16> gMsgParams;
+// CircularBuffer<char,gcMaxDataPartLen> gParseBuffer;
+char gParseBuffer[gcMaxDataPartLen] = {};
+byte gnParseBufferCnt = 0;
 
 // Serial Communication recive process
 void procSERReceive()
@@ -79,56 +111,134 @@ void procParseMsg()
     switch (gProcessState)
     {
       case STATE_CLEAN_UP:
+      #ifdef _DEBUG_
+        digitalWrite(pinLED_R,0);
+        digitalWrite(pinLED_G,0);
+        digitalWrite(pinLED_B,0);
+      #endif
         // TODO: Clean up message variables and switch to idle in the same loop
         gMsgCheckSum = 0;
         gMsgLength = 0;
         gMsgCmd = 0;
         gMsgParams.clear();
+        gnParseBufferCnt = 0;
         gProcessState = STATE_IDLE;
         
       case STATE_IDLE:
-        if (nData == 0xF0) gProcessState = STATE_CMD_LEN;   // TODO: Change SOP byte
+        #ifdef _DEBUG_
+          digitalWrite(pinLED_B,1);
+        #endif
+        if (nData == gcStartChar) gProcessState = STATE_CMD_LEN;
         // else discard data not part of package
         break;
       
       case STATE_CMD_LEN:
-        gMsgLength = nData;
-        gProcessState = STATE_CMD_ID;
+        if ((nData == gcPreIDChar)||(nData == gcPreCMDChar))
+        {
+          // length recived
+          gParseBuffer[gnParseBufferCnt++]=0;
+          // process buffer and switch to next state
+          gMsgLength = String(gParseBuffer).toInt();
+          #ifdef _DEBUG_
+            Serial.print("MsgLen = ");
+            Serial.println(gMsgLength);
+          #endif
+          gnParseBufferCnt = 0;
+          if (nData == gcPreCMDChar) gProcessState = STATE_CMD_NAME;
+          else gProcessState = STATE_CMD_ID;
+        }
+        else
+        {
+          gParseBuffer[gnParseBufferCnt++]=nData;
+        }
         break;
 
       case STATE_CMD_ID:
         // currently ignoring the ID
-        gProcessState = STATE_CMD_NAME;
+        if (nData == gcPreCMDChar) gProcessState = STATE_CMD_NAME;
         break;
 
       case STATE_CMD_NAME:
-        gMsgCmd = nData;
         // start calculating checksum
-        gMsgCheckSum = nData;
-        gProcessState = STATE_CMD_PARAMS;
+        gMsgCheckSum ^= nData;
+        if (nData == gcPreParamChar)
+        {
+          gParseBuffer[gnParseBufferCnt++]=0;
+          // process buffer and switch to next state
+          gMsgCmd = String(gParseBuffer).toInt();
+          gnParseBufferCnt = 0;
+          #ifdef _DEBUG_
+            Serial.print("MsgCmd = ");
+            Serial.println(gMsgCmd);
+          #endif
+          gProcessState = STATE_CMD_PARAMS;
+        }
+        else
+        {
+          gParseBuffer[gnParseBufferCnt++]=nData;
+        }
         break;
 
       case STATE_CMD_PARAMS:
-        gMsgParams.push(nData);
         // calculate checksum as XOR of all bytes
         gMsgCheckSum ^= nData;
-        if (nData == 0xF0) gProcessState = STATE_CMD_CS;     // TODO: Change end of params byte
+        if ((nData == gcParamSeparator)||(nData == gcPreChkSumChar)||(nData == gcStopChar))
+        {
+          gParseBuffer[gnParseBufferCnt++]=0;
+          // process buffer and switch to next state
+          gMsgParams.push(String(gParseBuffer).toInt());
+          gnParseBufferCnt = 0;
+          #ifdef _DEBUG_
+            Serial.print("Parameter ");
+            Serial.print(gMsgParams.size());
+            Serial.print(" = ");
+            Serial.println(gMsgParams.last());
+          #endif
+          if (nData == gcPreChkSumChar) gProcessState = STATE_CMD_CS;
+          if (nData == gcStopChar) gProcessState = STATE_CMD_OK;
+        }
+        else
+        {
+          gParseBuffer[gnParseBufferCnt++]=nData;
+        }
         break;
 
       case STATE_CMD_CS:
-        if ( gMsgCheckSum != nData)
+        if (nData == gcStopChar)
         {
-          // checksum ERROR, currently ignored
-          // gProcessState = STATE_CMD_ERROR;
+          #ifdef _DEBUG_
+          Serial.print("Checksum: ");
+          Serial.print(gMsgCheckSum);
+          #endif
+          gParseBuffer[gnParseBufferCnt++]=0;
+          if ( gMsgCheckSum == String(gParseBuffer).toInt())
+          {
+            gProcessState = STATE_CMD_OK;
+            #ifdef _DEBUG_
+              Serial.print(" == ");
+              Serial.println(String(gParseBuffer).toInt());
+              digitalWrite(pinLED_R,0);
+              digitalWrite(pinLED_G,1);
+              digitalWrite(pinLED_B,0);
+            #endif
+          }
+          else
+          {
+            gProcessState = STATE_CMD_ERROR;
+            #ifdef _DEBUG_
+              Serial.print(" != ");
+              Serial.println(String(gParseBuffer).toInt());
+              digitalWrite(pinLED_R,1);
+              digitalWrite(pinLED_G,0);
+              digitalWrite(pinLED_B,0);
+            #endif
+          }
         }
-        // else
-        gProcessState = STATE_CMD_END;
+        else
+        {
+          gParseBuffer[gnParseBufferCnt++]=nData;
+        }
         break;
-     
-     case STATE_CMD_END:
-        if (nData == 0xF0) gProcessState = STATE_CMD_OK;   // TODO: Change EOP byte
-        else gProcessState = STATE_CMD_ERROR;
-        break;   
 
       default:
         break;
@@ -136,8 +246,48 @@ void procParseMsg()
   }
 }
 
+void procExecMsg()
+{
+  switch (gProcessState)
+  {
+    case STATE_CMD_OK:
+      gSERTxBuffer.push('A');
+      gSERTxBuffer.push('C');
+      gSERTxBuffer.push('K');
+      gProcessState = STATE_CLEAN_UP;
+      break;
+      
+    case STATE_CMD_ERROR:
+      gSERTxBuffer.push('N');
+      gSERTxBuffer.push('A');
+      gSERTxBuffer.push('K');
+      gProcessState = STATE_CLEAN_UP;
+      break;
+    
+    default:
+      break;
+  }
+}
+
 void setup() 
 {
+  // initialize debug LED pins and test them
+  pinMode(pinLED_R,OUTPUT);
+  pinMode(pinLED_G,OUTPUT);
+  pinMode(pinLED_B,OUTPUT);
+
+#ifdef _DEBUG_INIT
+  digitalWrite(pinLED_R,1);
+  delay(500);
+  digitalWrite(pinLED_R,0);
+  digitalWrite(pinLED_G,1);
+  delay(500);
+  digitalWrite(pinLED_G,0);
+  digitalWrite(pinLED_B,1);
+  delay(500);
+  digitalWrite(pinLED_B,0);
+ #endif
+ 
   // start serial port
   Serial.begin(9600);  
 }
@@ -147,5 +297,6 @@ void loop()
   // main loop
   procSERReceive();
   procParseMsg();
+  procExecMsg();
   procSERSend();
 }
