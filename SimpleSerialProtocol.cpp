ec076e667abd0f7e23b9b1cca959e68a4242bdee
Ales@Zver2017 <ales.rosman@gmail.com>
2018-02-27 12:40:56 +0100

    Preparing for library
    Library framework prepared.

---
+++ SimpleSerialProtocol.cpp
@@ -0,0 +1,11 @@
+/*
+  SimpleSerialProtocol.cpp
+*/
+
+#include "Arduino.h"
+#include "SimpleSerialProtocol.h"
+
+SSP::SSP()
+{
+  // constructor
+}
---
+++ SimpleSerialProtocol.h
@@ -0,0 +1,17 @@
+/*
+  SimpleSerialProtocol.h
+*/
+#ifndef SimpleSerialProtocol_h
+#define SimpleSerialProtocol_h
+
+#include "Arduino.h"
+
+class SSP
+{
+  public:
+    SSP();              // constructor
+  private:
+    int _pin;           // private variables
+};
+
+#endif
